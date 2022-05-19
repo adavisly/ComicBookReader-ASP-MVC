a6d0f3436719ba50f053f1beb9568356632006d9
adavisly <webayesian@gmail.com>
2022-05-19 13:52:34 +0300

    added class of initial data

---
+++ ComicBookReader/ComicBookReader/Data/InitialData.cs
@@ -0,0 +1,25 @@
+﻿using ComicBookReader.Models;
+using System.Linq;
+
+namespace ComicBookReader.Data
+{
+    public static class InitialData
+    {
+        public static void Initialize(ComicBookReaderContext context)
+        {
+            if (!context.ComicBooks.Any())
+            {
+                context.ComicBooks.AddRange(
+                    new ComicBook
+                    {
+                        ComicBookTitle = "Аватар: Легенда об Аанге - Поиск",
+                        ComicBookLanguage = "English",
+                        ComicBookCover = "",
+                        ComicBookDescription = "Зуко и Азула ищут свою мать Урсу вместе с командой Аватара, которая пытается" +
+                        "держать психически неуравновешенную принцессу под контролем",
+                        ComicBookYear = 2014
+                    });
+            }
+        }
+    }
+}
